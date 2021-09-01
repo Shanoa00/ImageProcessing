3d6e6ecd9e1454fb90b3ef8f300ab8d171f162a0
Shanoa00 <mauriciobermudez00@gmail.com>
2021-09-01 20:41:10 +0900

    hw 1 finished :D

--- Image segmentation/BEP_1.py
+++ Image segmentation/BEP_1.py
@@ -14,7 +14,7 @@ def read_img(image):
     #cv.imshow("Color", lena) # Show Img
     #print("Original Size: ", lena.shape)
     #print("pixel value [10,10]: ", lena[10,10])
-    return img #,lena_gray
+    return img #,lena_img
 
 def green():
     try:
@@ -32,25 +32,25 @@ def green():
 
 
 #Assaigment 1:----------------------------------
-def my_gray(image):
+def my_img(image):
     try:
         lena = read_img(image)
     except:
         print("Check image path!")
-    gray= np.zeros((lena.shape[0],lena.shape[1],3), dtype= np.uint8)
+    img= np.zeros((lena.shape[0],lena.shape[1],3), dtype= np.uint8)
     b, g, r = lena[:,:,0], lena[:,:,1], lena[:,:,2]
     avg = 0.2989 * r + 0.5870 * g + 0.1140 * b
     #print(avg.shape)
     for i in range(3):
-        gray[:,:,i]= avg
-    cv.imshow("My_Gray", gray)
-    #print("Random pixel value (My gray): ", gray[10,83,0])
+        img[:,:,i]= avg
+    cv.imshow("My_img", img)
+    #print("Random pixel value (My img): ", img[10,83,0])
     #-----Comparing with function---
-    #lena_gray = cv.imread("lena.jpg", cv.IMREAD_GRAYSCALE)
-    #cv.imshow("Original_Gray", lena_gray)
-    #print("Same pixel value (Original gray): ", lena_gray[10,83])
+    #lena_img = cv.imread("lena.jpg", cv.IMREAD_imgSCALE)
+    #cv.imshow("Original_img", lena_img)
+    #print("Same pixel value (Original img): ", lena_img[10,83])
 
-    return gray
+    return img
 
 def draw_circle(x,y):
     img = read_img()
@@ -60,9 +60,9 @@ def draw_circle(x,y):
 
 #Assigment 2: Histogram----------------------------
 def histogram(image):
-    gray= my_gray(image)
-    #print(gray.shape)
-    count= Counter(gray[:,:,0].flatten())
+    img= my_img(image)
+    #print(img.shape)
+    count= Counter(img[:,:,0].flatten())
     #print(count.most_common())
     fig, (ax1, ax2) = plt.subplots(2)
     ax1.bar(count.keys(), count.values())
@@ -70,7 +70,7 @@ def histogram(image):
     
     ##sliding:
     offset= 35
-    of_img= gray[:,:,0]+ offset
+    of_img= img[:,:,0]+ offset
     ##Set max & min limits: 
     of_img = np.clip(of_img,0,255)
     cv.imshow("Offset", of_img)
@@ -80,16 +80,16 @@ def histogram(image):
     plt.show()
 
 def stretching():
-    gray= my_gray()
-    print(gray.shape)
-    stret_img = np.zeros((gray.shape[0],gray.shape[1]),dtype = 'uint8')
+    img= my_img()
+    print(img.shape)
+    stret_img = np.zeros((img.shape[0],img.shape[1]),dtype = 'uint8')
     # Loop over the image and apply Min-Max formulae
-    for i in range(gray.shape[0]):
-        for j in range(gray.shape[1]):
-            stret_img[i,j] = 255*(gray[i,j,0]-np.min(gray))/(np.max(gray)-np.min(gray))
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            stret_img[i,j] = 255*(img[i,j,0]-np.min(img))/(np.max(img)-np.min(img))
     cv.imshow("Stretching", stret_img)
 
-    count= Counter(gray[:,:,0].flatten())
+    count= Counter(img[:,:,0].flatten())
     #print(count.most_common())
     fig, (ax1, ax2) = plt.subplots(2)
     ax1.bar(count.keys(), count.values())
@@ -125,7 +125,7 @@ def enhance_contrast(image_matrix, bins=256):
 
 def equalize(image):
     try:
-        image_src = my_gray(image)
+        image_src = my_img(image)
     except:
         print("Check image path!")
     image_eq = enhance_contrast(image_matrix=image_src)
@@ -147,63 +147,120 @@ def threshold(image, thres):
     except:
         print("Check image path!")
     thres_img =  np.zeros((image_src.shape[0],image_src.shape[1]), dtype= np.uint8)
-    for i in range(image_src.shape[0]):
-        for j in range(image_src.shape[1]):
-            if image_src[i,j]>thres:
-                thres_img[i,j]= 1
+    # for i in range(image_src.shape[0]):
+    #     for j in range(image_src.shape[1]):
+    #         if image_src[i,j]>thres:
+    #             thres_img[i,j]= 1
+    ##faster way:
+    thres_img[image_src > thres] = 1
+
     #print(thres_img[100])
     fig=plt.figure()
     fig.add_subplot(1, 2, 1)   # subplot one
-    plt.imshow(image_src, cmap=plt.cm.gray)
+    plt.imshow(image_src, cmap='gray')
     fig.add_subplot(1, 2, 2)   # subplot two
-    # my data is OK to use gray colormap (0:black, 1:white)
-    plt.imshow(thres_img, cmap=plt.cm.gray)  # use appropriate colormap here
+    # my data is OK to use img colormap (0:black, 1:white)
+    plt.imshow(thres_img, cmap='gray')  # use appropriate colormap here
     plt.show()
+    
+def dynamic_tres(image, T0=13, maxIter=200):
+    img  = cv.imread(image,0)
 
-def dynamic_tres(image):
+    T = T0
+    #u1 = np.array(img[img <= T], dtype=np.float16).mean()
+    #print(np.array(img[img <= T], dtype=np.float16).mean())
+    #print((np.array(img[img <= T], dtype=np.float16).shape))
+    for i in range(0, maxIter):
+        u1 = np.array(img[img <= T], dtype=np.float16).mean()
+        u2 = np.array(img[img > T], dtype=np.float16).mean()
+        Tnew = (u1 + u2) / 2
+        
+        if abs(Tnew - T) < 0.01:
+            break
+        else:
+            T = Tnew
+    print(T)
+    threshold(image,T)
+
+def otsu(image):
+    import warnings
+
     try:
         img = cv.imread(image,0)
     except:
-        print("Check image path!")
-    hist, bins = np.histogram(img.flatten(), 256, [0, 256])
-    pdf = cv.calcHist([img], [0], None, [256], [0, 256])
-    #print(pdf)
-    plt.plot(pdf)
-    # plt.hist(img.ravel(),256,[0,256])
-    
-    plt.hist(img.flatten(), 256, [0, 256], color='r')
-    plt.plot(cdf * hist.max() / cdf.max(), color='b')
-    #plt.xlim([0, 256])
-    plt.legend(('cdf', 'histogram'), loc='upper left')
-    plt.show()
-    print()
-    
-def iterative(imgObj, T0=100, maxIter=200):
-        
-        img  = cv.imread(imgObj,0)
+        print("Check image path!")    
+    #img =np.array([1,2,1,3,4,5,6,8,9,1,2,3,4,2])
 
-        T = T0
-        u1 = np.array(img[img <= T], dtype=np.float16).mean()
-        print(np.array(img[img <= T], dtype=np.float16).mean())
-        print((np.array(img[img <= T], dtype=np.float16).shape))
-        for i in range(0, maxIter):
-            u1 = np.array(img[img <= T], dtype=np.float16).mean()
-            u2 = np.array(img[img > T], dtype=np.float16).mean()
-            Tnew = (u1 + u2) / 2
+    sigmas_btw = np.zeros((256,))
+    sigmas_wtn = np.zeros((256,))
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore", category=RuntimeWarning)
+        for T in range(1,255):
+            c0= img[img <= T]
+            w0= sum(c0)/ np.sum(img)
+            u0= np.mean(c0)
+            v0= np.var(c0)
+
+            c1= img[img > T]
+            w1= 1- w0
+            u1= np.mean(c1)
+            v1= np.var(c1)
+
+            sigma_between = w0* w1* ((u0- u1)** 2)
+            sigma_within = w0 * v0 + w1 * v1
             
-            if abs(Tnew - T) < 0.01:
-                break
-            else:
-                T = Tnew
-                #print(T)
+            #sigmasNan[T]= np.array([np.isnan(sigma_between),np.isnan(sigma_within)]) 
+            sigmas_btw[T]= sigma_between
+            sigmas_wtn[T]= sigma_within
         
+        #print(sigmas_wtn)
+        sigmas_btw=sigmas_btw[~np.isnan(sigmas_btw)]  
+        sigmas_wtn=sigmas_wtn[~np.isnan(sigmas_wtn)] 
+        
+        thresh_btw = np.array(np.argmax(sigmas_btw))
+        thresh_wth = np.array(np.argmin(sigmas_wtn))
+        print("otsu: ", thresh_btw)
+        threshold(image,thresh_btw)
+        
+        
+    
+
+    ##Using Library:
+    # ret, imgf = cv.threshold(img, 0, 255, cv.THRESH_BINARY+cv.THRESH_OTSU)
+    # fig=plt.figure()
+    # fig.add_subplot(1, 2, 1)   # subplot one
+    # plt.imshow(img, cmap='gray')
+    # fig.add_subplot(1, 2, 2)   # subplot two
+    # # my data is OK to use img colormap (0:black, 1:white)
+    # plt.imshow(imgf, cmap='gray')  # use appropriate colormap here
+    # plt.show()
+
+    #threshold(image,Totsu)
+
+def otsuu(image):
+    try:
+        img = cv.imread(image,0)
+    except:
+        print("Check image path!")    
+    ret, imgf = cv.threshold(img, 0, 255, cv.THRESH_BINARY+cv.THRESH_OTSU)
+    fig=plt.figure()
+    fig.add_subplot(1, 2, 1)   # subplot one
+    plt.imshow(img, cmap='gray')
+    fig.add_subplot(1, 2, 2)   # subplot two
+    # my data is OK to use img colormap (0:black, 1:white)
+    print('otsu: ',ret)
+    plt.imshow(imgf, cmap='gray')  # use appropriate colormap here
+    plt.show()
+
+    #threshold(image,ret)
+    
 
 
 
 def main():
     #green()
     #Asigment 1: 
-    #my_gray()
+    #my_img()
     #draw_circle(148,115)
 
     #Asigment 2:
@@ -212,7 +269,7 @@ def main():
     #equalize("landscape.jpg")
     #threshold("lena.jpg",100)
     #dynamic_tres("lena.jpg")
-    iterative("lena.jpg")
+    otsu("lena.jpg")
 
 if __name__=="__main__":
     main()
